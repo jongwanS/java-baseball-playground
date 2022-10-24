97972416ca6a0c98c4e7c923611053ad7d4c1230
Adios <30585897+jongwanS@users.noreply.github.com>
2022-10-24 23:10:18 +0900

    [야구게임] 1개의 숫자로만 ball/st/nothing 체크

---
+++ src/main/java/baseball/Ball.java
@@ -0,0 +1,37 @@
+package baseball;
+
+import java.util.Objects;
+
+public class Ball {
+
+    private final int position;
+    private final int ballNum;
+
+    public Ball(int position, int ballNum) {
+        this.position = position;
+        this.ballNum = ballNum;
+    }
+
+    public BallStatus play(Ball ball){
+        if(this.equals(ball)) return BallStatus.STRIKE;
+        if(ball.matchBallNum(ballNum))return BallStatus.BALL;
+        return BallStatus.NOTHING;
+    }
+
+    private boolean matchBallNum(int ballNum) {
+        return this.ballNum == ballNum;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ball ball = (Ball) o;
+        return position == ball.position && ballNum == ball.ballNum;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position, ballNum);
+    }
+}
